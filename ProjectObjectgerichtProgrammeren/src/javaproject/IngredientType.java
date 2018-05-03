e8df02f53eed96165ea59ac9a26b7cbaa3f5c5a5
axytho <jojojo.jonas888@hotmail.com>
2018-05-03 16:54:34 +0200

    Added the specifications for isValidSimpleName()

--- ProjectObjectgerichtProgrammeren/src/javaproject/IngredientType.java
+++ ProjectObjectgerichtProgrammeren/src/javaproject/IngredientType.java
@@ -2,8 +2,8 @@ package javaproject;
 
 
 /**
- * @invar	Name must be a valid name
- * 			| isValidName(getName())
+ * @invar	Simple Name must be a valid name
+ * 			| isValidSimpleName(getName())
  * 
  * @author Jonas
  *
@@ -15,13 +15,15 @@ public class IngredientType {
 	 * @param	name
 	 * 			| The name we want to give our Alchemic Ingredient
 	 * @return	True if
-	 * 			1) The name, if it consists of multiple words, must be seperated by spaces
+	 * 			1) The name, if it consists of multiple words, must be separated by spaces
 	 * 			2) Words may not contain numbers and characters other than ' and )
 	 * 			3) A single word must contain at least 3 letters, multiple words at least 2
-	 * 			4) 
+	 * 			4) Words must start with a Capital or a special character
+	 * 			5) mixed and with are exceptions: they do not require a capital but are not allowed in simple names
+	 * 			6) 
 	 */
 	
-	public static boolean isValidName(String name) {
+	public static boolean isValidSimpleName(String name) {
 		return false;
 	}
 
