6cc7dcd9cf1ef12e0fc5ade8cb7c15d1e39848f4
axytho <jojojo.jonas888@hotmail.com>
2018-05-03 13:54:27 +0200

    First commit

---
+++ ProjectObjectgerichtProgrammeren/src/javaproject/AlchemicIngredient.java
@@ -0,0 +1,15 @@
+package javaproject;
+
+/**
+ * @invar	Volality must be valid
+ * 			| isValidVolality(getVolality())
+ * 
+ * @author Jonas
+ *
+ */
+
+public class AlchemicIngredient {
+	
+
+
+}
---
+++ ProjectObjectgerichtProgrammeren/src/javaproject/IngredientType.java
@@ -0,0 +1,28 @@
+package javaproject;
+
+
+/**
+ * @invar	Name must be a valid name
+ * 			| isValidName(getName())
+ * 
+ * @author Jonas
+ *
+ */
+public class IngredientType {
+	
+	/**
+	 * 
+	 * @param	name
+	 * 			| The name we want to give our Alchemic Ingredient
+	 * @return	True if
+	 * 			1) The name, if it consists of multiple words, must be seperated by spaces
+	 * 			2) Words may not contain numbers and characters other than ' and )
+	 * 			3) A single word must contain at least 3 letters, multiple words at least 2
+	 * 			4) 
+	 */
+	
+	public static boolean isValidName(String name) {
+		return true;
+	}
+
+}
